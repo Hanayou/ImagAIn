fc26526a6d2b63353923fa89290c59b40f887780
Hanayou <samuel.short@gmail.com>
2023-10-26 03:22:47 +0100

    Implement basic server.js with express API routes and OpenAI API integration

---
+++ server.js
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// OpenAI API config setup
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI,
+});
+
+// Express server setup
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/imagAIn', async (req, res) => {
+
+    const prompt = req.body.prompt;
+
+    const aiResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+    });
+
+    const image = aiResponse.data.data[0].url;
+    res.send({ image });
+});
+
+app.listen(8080, () => console.log('makeart on http://localhost:8080/imagAIn'));
